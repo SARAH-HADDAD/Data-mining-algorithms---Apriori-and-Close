c0d19bf344780b64d381992d601c1726cc2201f2
Haddad Sarah <haddads686@gmail.com>
2023-03-29 15:38:39 +0100

    update

--- script.py
+++ script.py
@@ -2,36 +2,46 @@ from itertools import combinations
 # This function loads the transactional data from a CSV file and converts each transaction into a set of items in order:
 import csv
 
-import csv
-
 def load_data(file_path):
     data = []
+    prices = []  # add this list to store all the "Price in USD" values
     with open(file_path, 'r') as f:
         reader = csv.reader(f)
         header = next(reader) # récupérer la première ligne en tant qu'en-tête
         for row in reader:
             data.append(row)
-    return data
+            prices.append(float(row[2])) 
+    return data, prices 
 
 
 # This function counts the number of times each item appears in the dataset:
-def get_item_counts(data):
+def get_item_counts(data,average_price):
     item_counts = {}
+    item_importance = {}
     for transaction in data:
-        print(transaction)
-        print("-")
+        #print(transaction[2])
+        if float(transaction[2]) > average_price:
+            importance=1
+        else:
+            importance=0
         for item in transaction:
             if item in item_counts:
                 item_counts[item] += 1
+                item_importance[item] += importance
             else:
                 item_counts[item] = 1
-    return item_counts
+                item_importance[item] = importance
+    return item_counts,item_importance
 # This function generates all frequent itemsets in the dataset that have a support greater than or equal to the specified minimum support:
-def get_frequent_itemsets(data, min_support):
-    item_counts = get_item_counts(data)
+def get_frequent_itemsets(data, min_support,average_price):
+    item_counts,item_importance = get_item_counts(data,average_price)
+    #print(item_counts)
     num_transactions = len(data)
     min_support_count = num_transactions * min_support
     frequent_itemsets = [frozenset({item}) for item, count in item_counts.items() if count >= min_support_count]
+    frequentximportant_itemsets = [frozenset({item}) for item, count in item_counts.items() if count >= min_support_count or item_importance.get(item, 0) >= 1]
+    print('frequentximportant_itemsets:',frequentximportant_itemsets)
+    print('frequent_itemsets:',frequent_itemsets)
     # within each loop iteration, it generates all possible combinations of itemsets of size k
     k = 2
     while frequent_itemsets:
@@ -72,16 +82,16 @@ def get_association_rules(frequent_itemsets, min_confidence, data):
     return association_rules
 
 # Load the data
-data = load_data('test.csv')
-
+data, prices = load_data('test.csv')
+average_price = sum(prices) / len(prices)
+print(f"The average price in USD is {average_price:.2f}")
 # Set the minimum support and confidence
-min_support = 0.2
-
+min_support = 0.5
 min_confidence = 0.5
 print(f"Règles d'association pour min_confidence = {min_confidence}:")
 print(f"Règles d'association pour min_support = {min_support}:")
 # Find the frequent itemsets
-frequent_itemsets = get_frequent_itemsets(data, min_support)
+frequent_itemsets = get_frequent_itemsets(data, min_support,average_price)
 # Trouver les règles d'association avec la valeur actuelle de min_confidence
 association_rules = get_association_rules(frequent_itemsets, min_confidence, data)
 # Imprimer les règles d'association et leur performance
