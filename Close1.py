5b72a1f67d248bce508b290c0069cd3331ff4eb5
Haddad Sarah <haddads686@gmail.com>
2023-04-16 13:59:29 +0100

    mzl el khir

--- Close1.py
+++ Close1.py
@@ -32,16 +32,22 @@ k = 1
 
 # tant que ensemble de candidats est non vide faire
 while len(candidates) > 0:
-    # Calculer le support des candidats
+    #1) Calculer le support des candidats
+    #2) Élaguer l’ensemble de candidats par rapport à minsup
+    Elaguer = []
     for candidate in candidates:
         for i in range(len(candidate)):
             if i == 0:
                 support = itemset[candidate[i]]
             else:
                 support = support & itemset[candidate[i]]
-        support = np.sum(support)
-        print('candidate:', candidate)
-        print('support:', support)
+        support = np.sum(support)/len(data)
+        if support < minsup:
+            Elaguer.append(candidate)
+            candidates.remove(candidate)
+        #print('candidate:', candidate)
+        #print('support:', support)
+    #print('candidates:', candidates)
 
     k += 1
     new_combinations = generate_combinations(base, k)
@@ -49,13 +55,19 @@ while len(candidates) > 0:
         
     if candidates != []:
         for c in new_combinations:
+            #print('Elaguer:',Elaguer)
+            # if c contais a subset of Elaguer
+            for x in Elaguer:
+                print('x:', x)
+                # if all elements of x are in c
+                if all(elem in c for elem in x):
+                    print("c contais a subset of Elaguer")
+                    print('c:', c)                
+            c_list = list(c)
             new_candidates.append(list(c))
-        print('_'*20)    
-        print('new_candidates:', new_candidates)
-        print('_'*20)
+
+        #print('_'*20)    
+        #print('new_candidates:', new_candidates)
+        #print('_'*20)
 
     candidates = new_candidates
-
-
-
-
