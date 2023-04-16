a81ded9bf725630214d276cb39991fad28b2a17a
Haddad Sarah <haddads686@gmail.com>
2023-04-16 17:06:12 +0100

    cv cv

--- Close1.py
+++ Close1.py
@@ -1,7 +1,6 @@
 import numpy as np
 from itertools import combinations
 
-
 data = [['Apple', 'Banana', 'Lemon', 'Kiwi', 'Fig'],
        ['Apple', 'Banana', 'Lemon', 'Kiwi'],
        ['Pear', 'Fig'],
@@ -15,51 +14,41 @@ items = sorted(set([item for transaction in data for item in transaction]))
 itemset = {}
 for item in items:
     itemset[item] = np.array([1 if item in transaction else 0 for transaction in data])
-#print(itemset)
+
+# candidates initiaux
 candidates = list(itemset.keys())
+candidates = [[c] for c in candidates]
 
-# mettre chaque candidat dans une liste imbriquée
-for i in range(len(candidates)):
-    candidates[i] = [candidates[i]]
-
-#print(candidates)
-
-base = list(itemset.keys())
-#minsup = 2/5
 minsup = 2
 k = 1
 
-# tant que ensemble de candidats est non vide faire
 while len(candidates) > 0 and k < 4:
     print('Generation: ', k)
     print('candidates:', candidates)
-    # Calculer le support des candidats
+    # calculer le support des candidats
     frequent_itemsets = []
     for candidate in candidates:
-        support = []
-        for i in range(len(candidate)):
-            if i == 0:
-                support = itemset[candidate[i]]
-            else:
-                support = support & itemset[candidate[i]]
-        #support = np.sum(support)/len(data)
+        support = itemset[candidate[0]]
+        for i in range(1, len(candidate)):
+            support = support & itemset[candidate[i]]
         support = np.sum(support)
-        print('candidate:', candidate)
-        print('support:', support)
         if support >= minsup:
-            #print('remove:', candidate)
-            #candidates.remove(candidate)
             frequent_itemsets.append(candidate)
-    # Générer les candidats de taille k + 1
+
+    # Générer les candidats de taille k + 1 qui ne contiennent pas d'éléments inférieurs
     candidates = []
     for i in range(len(frequent_itemsets)):
-        for j in range(i+1, len(frequent_itemsets)):
-            if frequent_itemsets[i][:k-1] == frequent_itemsets[j][:k-1] and frequent_itemsets[i][k-1] < frequent_itemsets[j][k-1]:
-                candidate = frequent_itemsets[i] + [frequent_itemsets[j][k-1]]
-                if candidate not in frequent_itemsets and candidate not in candidates:
+        for j in range(i + 1, len(frequent_itemsets)):
+            # vérifier que les deux itemsets ont k-1 éléments en commun
+            if frequent_itemsets[i][: k - 1] == frequent_itemsets[j][: k - 1]:
+                # combiner les deux itemsets pour créer un nouveau candidat
+                candidate = frequent_itemsets[i] + [frequent_itemsets[j][-1]]
+                # vérifier que tous les k-1 itemsets de candidat sont fréquents
+                is_valid = True
+                for subset in combinations(candidate, k):
+                    if list(subset) not in frequent_itemsets:
+                        is_valid = False
+                        break
+                if is_valid:
                     candidates.append(candidate)
     k += 1
-
-
-
-
