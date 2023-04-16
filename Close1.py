a0001cf897d167887ea25915f6793c78a144ce2e
Haddad Sarah <haddads686@gmail.com>
2023-04-16 16:52:31 +0100

    cv cv

--- Close1.py
+++ Close1.py
@@ -1,55 +1,65 @@
-from collections import defaultdict
 import numpy as np
-def calculate_support(itemset, transactions):
-    count = 0
-    for transaction in transactions:
-        if set(itemset).issubset(set(transaction)):
-            count += 1
-    return count
+from itertools import combinations
 
-def calculate_closure(itemset, K):
-    closure = set(itemset)
-    i = 0
-    while i < len(K):
-        if set(K[i]).issubset(closure):
-            closure = closure.union(set(K[i]))
-            K.pop(i)
-        else:
-            i += 1
-    return closure
 
-def close_algorithm(candidate_itemsets, minsup, transactions):
-    frequent_closed_itemsets = set()
-    k=1
-    #print('Candidate itemsets: ', candidate_itemsets)
-    while candidate_itemsets != [] and k <= 3:
-        frequent_itemsets = []
-        print('Generation: ', k)
-        for item in candidate_itemsets:
-            print('Item: ',item)
-            support = calculate_support(item, transactions)
-            print('Support: ', support)
-            if support >= minsup:
-                frequent_itemsets.append(item)
-        
-        for i in range(len(frequent_itemsets)-1):
-            for j in range(i+1, len(frequent_itemsets)):
-                itemset = sorted(set(frequent_itemsets[i]).union(set(frequent_itemsets[j])))
-                if len(itemset) > len(frequent_itemsets[0]) and itemset not in candidate_itemsets:
-                    candidate_itemsets.append(itemset)
-        k=k+1
-    print('Frequent itemsets: ', frequent_itemsets)
-                    
-data = [['A', 'B', 'C', 'E'], ['A', 'B', 'C', 'D', 'E'], ['A', 'B'], ['C', 'E'], ['A', 'C', 'D']]
-candidate_itemsets = sorted(set([item for transaction in data for item in transaction]))
+data = [['Apple', 'Banana', 'Lemon', 'Kiwi', 'Fig'],
+       ['Apple', 'Banana', 'Lemon', 'Kiwi'],
+       ['Pear', 'Fig'],
+       ['Apple', 'Banana', 'Kiwi', 'Fig'],
+       ['Apple', 'Lemon', 'Kiwi']]
+
+# Créer un ensemble d'éléments unique
+items = sorted(set([item for transaction in data for item in transaction]))
+
 # Créer un dictionnaire avec les éléments comme clés et des vecteurs binaires correspondants
 itemset = {}
-for item in candidate_itemsets:
+for item in items:
     itemset[item] = np.array([1 if item in transaction else 0 for transaction in data])
 #print(itemset)
 candidates = list(itemset.keys())
+
+# mettre chaque candidat dans une liste imbriquée
+for i in range(len(candidates)):
+    candidates[i] = [candidates[i]]
+
+#print(candidates)
+
+base = list(itemset.keys())
+#minsup = 2/5
 minsup = 2
+k = 1
 
-frequent_closed_itemsets = close_algorithm(candidate_itemsets, minsup, data)
+# tant que ensemble de candidats est non vide faire
+while len(candidates) > 0 and k < 4:
+    print('Generation: ', k)
+    print('candidates:', candidates)
+    # Calculer le support des candidats
+    frequent_itemsets = []
+    for candidate in candidates:
+        support = []
+        for i in range(len(candidate)):
+            if i == 0:
+                support = itemset[candidate[i]]
+            else:
+                support = support & itemset[candidate[i]]
+        #support = np.sum(support)/len(data)
+        support = np.sum(support)
+        print('candidate:', candidate)
+        print('support:', support)
+        if support >= minsup:
+            #print('remove:', candidate)
+            #candidates.remove(candidate)
+            frequent_itemsets.append(candidate)
+    # Générer les candidats de taille k + 1
+    candidates = []
+    for i in range(len(frequent_itemsets)):
+        for j in range(i+1, len(frequent_itemsets)):
+            if frequent_itemsets[i][:k-1] == frequent_itemsets[j][:k-1] and frequent_itemsets[i][k-1] < frequent_itemsets[j][k-1]:
+                candidate = frequent_itemsets[i] + [frequent_itemsets[j][k-1]]
+                if candidate not in frequent_itemsets and candidate not in candidates:
+                    candidates.append(candidate)
+    k += 1
 
-print(frequent_closed_itemsets)
+
+
+
